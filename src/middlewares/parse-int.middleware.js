9c4b3bbab8261bed59f26ff2e20032d26d2f8658
Ian Serrano <yenienserrano75@gmail.com>
2021-07-16 18:39:22 +0200

    [ADD] pagination added

---
+++ src/middlewares/parse-int.middleware.js
@@ -0,0 +1,15 @@
+module.exports = function (req, res, next) {
+  const queryString = req.query;
+
+  for (const key in queryString) {
+    const length = queryString[key].length;
+    const isValid = length > 20 ? false : !isNaN(parseInt(queryString[key]));
+
+    if (isValid) {
+      queryString[key] = parseInt(queryString[key]);
+    }
+  }
+
+  req.query = queryString;
+  next();
+};
